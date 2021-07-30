b4c5154cfdde7b3fc48d251d1d0f1011275ea72e
James Bowes <jbowes@repl.ca>
2021-07-30 07:54:46 -0300

    Add cut

---
+++ internal/install/builtin/cut.go
@@ -0,0 +1,34 @@
+package builtin
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/spf13/pflag"
+)
+
+func Cut(ctx context.Context, host Host, ios IOs, args []string) error {
+	fs := pflag.NewFlagSet("", pflag.ContinueOnError)
+	delim := fs.StringP("", "d", "\t", "")
+	field := fs.IntP("f", "f", 1, "") // TODO: multi field support
+	err := fs.Parse(args)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	host.Log("cut")
+
+	buf, err := io.ReadAll(ios.In)
+	if err != nil {
+		return err
+	}
+
+	parts := strings.Split(string(buf), *delim)
+	_, err = ios.Out.Write([]byte(parts[*field-1]))
+	return err
+}
+
+func init() { Builtin["cut"] = Cut }
