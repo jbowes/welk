7750425cf9bcd57092d32bc0bb6f1ef128beed08
James Bowes <jbowes@repl.ca>
2021-08-01 17:02:12 -0300

    Add basic support for uname -m

--- internal/install/builtin/uname.go
+++ internal/install/builtin/uname.go
@@ -4,24 +4,44 @@ import (
 	"context"
 	"fmt"
 	"runtime"
+
+	"github.com/spf13/pflag"
 )
 
-var unamed = map[string]string{
+var unamedOS = map[string]string{
 	"darwin": "Darwin",
+}
+
+var unamedArch = map[string]string{
+	"amd64": "x86_64",
 }
 
 func Uname(ctx context.Context, host Host, ios IOs, args []string) error {
 	// TODO: uname could prompt for non m and non s args?
+	fs := pflag.NewFlagSet("", pflag.ContinueOnError)
+	arch := fs.BoolP("m", "m", false, "")
+	os := fs.BoolP("s", "s", false, "")
+	err := fs.Parse(args)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
 
 	host.Log("uname")
 
-	if len(args) == 0 || args[0] == "-s" {
-		output := unamed[runtime.GOOS]
+	// TODO: args can be supplied at once and have a specific order for printing
+	switch {
+	case len(args) == 0, *os:
+		output := unamedOS[runtime.GOOS]
+		_, err := fmt.Fprint(ios.Out, output)
+		return err
+	case *arch:
+		output := unamedArch[runtime.GOARCH]
 		_, err := fmt.Fprint(ios.Out, output)
 		return err
 	}
-	return nil
 
+	return nil
 }
 
 func init() { Builtin["uname"] = Uname }
