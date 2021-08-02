260812ff3c3300102bfc14333a021c5767180622
James Bowes <jbowes@repl.ca>
2021-08-02 07:29:38 -0300

    More curl args support

--- internal/install/builtin/curl.go
+++ internal/install/builtin/curl.go
@@ -7,7 +7,9 @@ import (
 	"net/http"
 	"net/url"
 	"path"
+	"strings"
 
+	"github.com/jbowes/sumdog/internal/install/devnull"
 	"github.com/spf13/pflag"
 )
 
@@ -18,8 +20,13 @@ func Curl(ctx context.Context, host Host, ios IOs, args []string) error {
 	fs.BoolP("location", "L", false, "") // Ignored, as go follows Location headers, but maybe it shoudln't be
 
 	rname := fs.BoolP("remote-name", "O", false, "")
+	lname := fs.StringP("output", "o", "", "")
+
+	outfmt := fs.StringP("write-out", "w", "", "")
+
 	fs.BoolP("fail", "f", false, "")        // TODO: use this to make non-success responses return errors
 	fs.StringSliceP("header", "H", nil, "") // TODO: use this
+
 	err := fs.Parse(args)
 	if err != nil {
 		fmt.Println(err)
@@ -35,7 +42,14 @@ func Curl(ctx context.Context, host Host, ios IOs, args []string) error {
 	defer resp.Body.Close()
 
 	wc := ios.Out
-	if *rname {
+
+	// If we have an output string, by default drop the output.
+	// the remote name flag will overwrite this.
+	if *outfmt != "" {
+		wc = devnull.New()
+	}
+
+	if *rname && *lname == "" {
 		u, err := url.Parse(fs.Arg(0))
 		if err != nil {
 			return err
@@ -43,8 +57,22 @@ func Curl(ctx context.Context, host Host, ios IOs, args []string) error {
 
 		wc = host.Write(path.Base(u.Path))
 	}
+	if *lname != "" {
+		wc = host.Write(*lname)
+	}
 
-	_, err = io.Copy(wc, resp.Body)
+	if _, err = io.Copy(wc, resp.Body); err != nil {
+		return fmt.Errorf("could not copy output: %w", err)
+	}
+
+	if *outfmt == "" {
+		return nil
+	}
+
+	// TODO: more robust handling here.
+	out := *outfmt
+	out = strings.ReplaceAll(out, "%(http_code)", fmt.Sprintf("%d", resp.StatusCode))
+	_, err = ios.Out.Write([]byte(out))
 	return err
 }
 
