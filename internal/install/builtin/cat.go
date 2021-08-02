6152974fce78467a052dc995c08d2f3356c9551b
James Bowes <jbowes@repl.ca>
2021-08-02 14:18:29 -0300

    Teach cat how to read from files and output a little better

--- internal/install/builtin/cat.go
+++ internal/install/builtin/cat.go
@@ -2,19 +2,43 @@ package builtin
 
 import (
 	"context"
+	"fmt"
+	"io"
 	"io/ioutil"
+
+	"github.com/jbowes/sumdog/internal/install/devnull"
 )
 
 func Cat(ctx context.Context, host Host, ios IOs, args []string) error {
 	// TODO: support all the args and files and stuff, instead of just stdin.
 
-	b, err := ioutil.ReadAll(ios.In)
-	if err != nil {
-		return err
+	var b []byte
+	var err error
+
+	if len(args) == 0 {
+		b, err = ioutil.ReadAll(ios.In)
+		if err != nil {
+			return err
+		}
+	} else {
+		for _, f := range args {
+			// used for a multi-line comment trick in goreleaser / go downloader scripts
+			if f == "/dev/null" {
+				continue
+			}
+			fmt.Println(f)
+			// TODO: check for existance and error?
+			b = append(b, host.File(f)...)
+		}
 	}
 
-	// TODO: send to stdout if appropriate.
-	host.Log("cat", string(b))
+	// TODO: this isn't accurate. it should log when going to the controlling
+	// console (which happens do be IsDevNull right now).
+	if devnull.IsDevNull(ios.Out) {
+		host.Log("cat", string(b))
+	} else {
+		io.WriteString(ios.Out, string(b))
+	}
 
 	return nil
 }
