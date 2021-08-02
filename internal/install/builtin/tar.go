48553bf9ce6947861a07affafb96d5eba68f0ed9
James Bowes <jbowes@repl.ca>
2021-08-02 12:45:16 -0300

    Implement tar

---
+++ internal/install/builtin/tar.go
@@ -0,0 +1,71 @@
+package builtin
+
+import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"context"
+	"errors"
+	"fmt"
+	"io"
+
+	"github.com/spf13/pflag"
+)
+
+func Tar(ctx context.Context, host Host, ios IOs, args []string) error {
+	// TODO: supported bundled args? eg tar zxf instead of tar -zxf
+
+	fs := pflag.NewFlagSet("", pflag.ContinueOnError)
+	gz := fs.BoolP("z", "z", false, "")
+	extract := fs.BoolP("x", "x", false, "")
+	fname := fs.StringP("f", "f", "", "")
+	err := fs.Parse(args)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	host.Log("tar")
+
+	if !*extract {
+		return errors.New("tar: only extract is supported")
+	}
+
+	in := ios.In
+	if *fname != "" {
+		in = bytes.NewReader(host.File(*fname))
+	}
+
+	if *gz {
+		in, err = gzip.NewReader(in)
+		if err != nil {
+			return err
+		}
+	}
+
+	tr := tar.NewReader(in)
+	for {
+		hdr, err := tr.Next()
+		if err == io.EOF {
+			break // End of archive
+		}
+		if err != nil {
+			return err
+		}
+
+		err = func() error {
+			o := host.Write(hdr.Name)
+			defer o.Close()
+
+			_, err := io.Copy(o, tr)
+			return err
+		}() // get that defer
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func init() { Builtin["tar"] = Tar }
