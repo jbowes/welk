640ef0345e90ebf54aa6f4aafce84dcfe7305f6a
James Bowes <jbowes@repl.ca>
2021-08-12 18:29:02 -0300

    Add update checks
    once a week welk will check for a new release. For any run, it will notify on
    it. Brought to you by jbowes/whatsnew(tm)
    
    Closes #8

--- go.mod
+++ go.mod
@@ -7,9 +7,10 @@ require (
 	github.com/benhoyt/goawk v1.7.0
 	github.com/charmbracelet/bubbletea v0.14.1
 	github.com/charmbracelet/lipgloss v0.3.0
-	github.com/containerd/console v1.0.2 // indirect
+	github.com/containerd/console v1.0.3 // indirect
 	github.com/creack/pty v1.1.14
 	github.com/google/uuid v1.3.0
+	github.com/jbowes/whatsnew v0.4.0
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.9.0 // indirect
 	github.com/rwtodd/Go.Sed v0.0.0-20190103233418-906bc69c9394
@@ -18,7 +19,7 @@ require (
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/mod v0.4.2
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
-	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069
+	golang.org/x/sys v0.0.0-20210809222454-d867a43fc93e
 	golang.org/x/term v0.0.0-20210615171337-6886f2dfbf5b
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 	mvdan.cc/sh/v3 v3.3.1
--- internal/cmd/root.go
+++ internal/cmd/root.go
@@ -4,7 +4,17 @@
 
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"context"
+	"fmt"
+	"path/filepath"
+
+	"github.com/adrg/xdg"
+	"github.com/charmbracelet/lipgloss"
+	"github.com/jbowes/welk/internal/diagnostics"
+	"github.com/jbowes/whatsnew"
+	"github.com/spf13/cobra"
+)
 
 // TODO: have this run install by default
 var rootCmd = cobra.Command{
@@ -15,4 +25,25 @@ var rootCmd = cobra.Command{
 // TODO: add a -v, --version flag (though v could be for verbose)
 
 // TODO: handle err + panics here.
-func Execute() { _ = rootCmd.Execute() }
+func Execute() {
+	// TODO: make this as middleware and don't put it on every command.
+	d := diagnostics.New()
+	fut := whatsnew.Check(context.TODO(), &whatsnew.Options{
+		Slug:    "jbowes/welk",
+		Cache:   filepath.Join(xdg.CacheHome, "welk", "release-check.json"),
+		Version: d.Version,
+	})
+
+	_ = rootCmd.Execute()
+
+	if v, _ := fut.Get(); v != "" {
+
+		bold := lipgloss.NewStyle().Bold(true) // TODO: this doesn't work with non-terminals :(
+		s := lipgloss.NewStyle().Foreground(lipgloss.Color("#FF9999"))
+
+		fmt.Println(s.Render("💾 new release available:"), v)
+		fmt.Println("download from", bold.Render(
+			fmt.Sprintf("https://github.com/jbowes/welk/releases/tag/%s", v),
+		))
+	}
+}
